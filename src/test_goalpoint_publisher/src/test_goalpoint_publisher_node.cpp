1ee7b905fc83d7ca333ffa2e2fcc095d8d7a2d32
chenrrry <wonu19960717@163.com>
2023-12-06 20:11:21 +0800

    add border viz

--- src/test_goalpoint_publisher/src/test_goalpoint_publisher_node.cpp
+++ src/test_goalpoint_publisher/src/test_goalpoint_publisher_node.cpp
@@ -10,6 +10,11 @@
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
 #include "std_msgs/Empty.h"
 #include "std_msgs/Int32.h" 
+#include <nav_msgs/Path.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <string>
+#include <nav_msgs/OccupancyGrid.h>
+#include <tf/tf.h>
 
 
 class GoalPointPublisher
@@ -20,16 +25,31 @@ private:
     ros::Publisher pub_goal_;
     ros::Subscriber sub_;
 
+    ros::Subscriber subPath;
+    ros::Subscriber subPathNodes;
+    ros::Subscriber subPathVehicles;
+    ros::Subscriber subPath2DNodes;
+    ros::Subscriber subPathBoxes;
+
+    std::string test_file;
+
     std::vector<geometry_msgs::Point> points_;
     std::vector<geometry_msgs::PoseStamped> start_;
     std::vector<geometry_msgs::PoseStamped> goal_;
     size_t current_point_index_;
     ros::Time last_point_time_;
     std::ofstream outfile;
+    std::ofstream outfile_viz;
 
     void numberCallback(const std_msgs::Int32::ConstPtr& msg);
     void publishGoalPoint(const geometry_msgs::Point& start_point, const geometry_msgs::Point& goal_point);
     std::vector<geometry_msgs::Point> readPointsFromFile(const std::string& file_path);
+
+    void pathCallback(const nav_msgs::Path::ConstPtr& msg); 
+    void pathNodesCallback(const visualization_msgs::MarkerArray::ConstPtr& msg);
+    void pathVehiclesCallback(const visualization_msgs::MarkerArray::ConstPtr& msg);
+    void path2DNodesCallback(const visualization_msgs::MarkerArray::ConstPtr& msg);
+    void pathBoxesCallback(const visualization_msgs::MarkerArray::ConstPtr& msg);
 
 public:
     GoalPointPublisher();
@@ -48,12 +68,22 @@ GoalPointPublisher::GoalPointPublisher()
     pub_goal_ = nh_.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1);
     sub_ = nh_.subscribe("/start_notification", 1, &GoalPointPublisher::numberCallback, this);
 
+    subPath = nh_.subscribe("/sPath", 1, &GoalPointPublisher::pathCallback, this);
+    subPathNodes = nh_.subscribe("/sPathNodes", 1, &GoalPointPublisher::pathNodesCallback, this);
+    subPathVehicles = nh_.subscribe("/sPathVehicle", 1, &GoalPointPublisher::pathVehiclesCallback, this);
+    subPath2DNodes = nh_.subscribe("/path2DNodes", 1, &GoalPointPublisher::path2DNodesCallback, this);
+    subPathBoxes = nh_.subscribe("/pathBoxes", 1, &GoalPointPublisher::pathBoxesCallback, this);
+    
+    test_file="test_map_thin_1m_to_10pixel_1_cry";
+
     std::string pkg_path = ros::package::getPath("test_goalpoint_publisher");
     std::cout << "current package : " << pkg_path;
-    std::string points_file_path = pkg_path + "/data/tunnel_test.txt";
+    std::string points_file_path = pkg_path + "/data/"+test_file+".txt";
     std::vector<geometry_msgs::Point> points_from_file = readPointsFromFile(points_file_path);
-    std::string output_file_path = pkg_path + "/data/tunnel_test_result.txt";; 
+    std::string output_file_path = pkg_path + "/data/"+test_file+"_result.txt";
     outfile.open(output_file_path);
+
+    outfile<<"111 "<<output_file_path<<std::endl;
     
     for (const auto& point : points_from_file)
     {
@@ -109,6 +139,14 @@ void GoalPointPublisher::numberCallback(const std_msgs::Int32::ConstPtr& msg) //
     
     int number = msg->data;
     outfile<<"data: "<<number<<std::endl;
+    if(number==-1){
+        outfile_viz.close();
+        std::string output_file_path_viz = ros::package::getPath("test_goalpoint_publisher") + 
+            "/data/"+test_file+"_result_"+std::to_string(current_point_index_/2)+".txt";; 
+        outfile_viz.open(output_file_path_viz);
+        outfile<<output_file_path_viz<<std::endl;
+        return;
+    }
     if (number==0)  // Threshold to consider "reached", you can adjust as needed
     {
         current_point_index_=0;
@@ -118,6 +156,7 @@ void GoalPointPublisher::numberCallback(const std_msgs::Int32::ConstPtr& msg) //
         << " in " << travel_time.toSec() << " seconds." << std::endl;
         current_point_index_=number*2;
     }
+
     outfile<<"cp"<<current_point_index_<<"size "<<points_.size() <<std::endl;
     last_point_time_ = ros::Time::now();
     publishNextGoalPoint(); 
@@ -142,12 +181,13 @@ void GoalPointPublisher::publishGoalPoint(const geometry_msgs::Point& start_poin
     start.pose.pose.position.x=start_point.x;
     start.pose.pose.position.y=start_point.y;
     start.pose.pose.position.z=0;
-    start.pose.pose.orientation.x=0;
-    start.pose.pose.orientation.y=0;
-    start.pose.pose.orientation.z=0;
-    start.pose.pose.orientation.w=1;
 
+    tf::Quaternion tf_quaternion1;
+    tf_quaternion1.setRPY(0,0,start_point.z); // 假设已经设置了roll, pitch, yaw
+    tf::quaternionTFToMsg(tf_quaternion1, start.pose.pose.orientation);
     pub_start_.publish(start);
+
+    outfile<<start.pose.pose.orientation.x<<start.pose.pose.orientation.y<<start.pose.pose.orientation.z<<start.pose.pose.orientation.w<<std::endl;
 
     geometry_msgs::PoseStamped goal;
     goal.header.frame_id = "map";
@@ -155,18 +195,87 @@ void GoalPointPublisher::publishGoalPoint(const geometry_msgs::Point& start_poin
     goal.pose.position.x=goal_point.x;
     goal.pose.position.y=goal_point.y;
     goal.pose.position.z=0;
-    goal.pose.orientation.x=0;
-    goal.pose.orientation.y=0;
-    goal.pose.orientation.z=0;
-    goal.pose.orientation.w=1;
+    tf::Quaternion tf_quaternion2;
+    tf_quaternion2.setRPY(0,0,goal_point.z); // 假设已经设置了roll, pitch, yaw
+    tf::quaternionTFToMsg(tf_quaternion2, goal.pose.orientation);
 
     usleep(10000); // small sleep before next publish
     pub_goal_.publish(goal);
 
     usleep(10000); // set to 1000000us (1s) on real robot
-    // pub_joy_.publish(joy);
+
     outfile<<"publish start and goal"<<std::endl;
 }
+
+void GoalPointPublisher::pathCallback(const nav_msgs::Path::ConstPtr& msg) {
+    // std::string output_file_path_viz = ros::package::getPath("test_goalpoint_publisher") + 
+    //     "/data/tunnel_test_result_"+std::to_string(current_point_index_/2)+".txt";; 
+    // outfile_viz.open(output_file_path_viz);
+    // outfile_viz.close();
+    // 处理路径数据
+    
+}
+
+void GoalPointPublisher::pathNodesCallback(const visualization_msgs::MarkerArray::ConstPtr& msg) {
+    outfile<<"1"<<std::endl;
+    for (const auto& marker : msg->markers) {
+        outfile_viz << "1" << std::endl;
+        outfile_viz << marker.pose.position.x << " "<< marker.pose.position.y << " " << marker.pose.position.z << std::endl;
+        outfile_viz << marker.scale.x << " " << marker.scale.y << " " << marker.scale.z << std::endl;
+        outfile_viz << marker.color.r << " "<< marker.color.r << " " << marker.color.r << " " << marker.color.a << std::endl;
+    }
+    // 记录3D路径节点信息
+    // Index为1，第一排为位置信息xyz，第二排为缩放信息xyz，第三排为颜色信息rgba
+}
+
+void GoalPointPublisher::pathVehiclesCallback(const visualization_msgs::MarkerArray::ConstPtr& msg) {
+    outfile<<"2"<<std::endl;
+    for (const auto& marker : msg->markers) {
+        if(marker.type==visualization_msgs::Marker::CUBE){
+            outfile_viz << "2" << std::endl;
+            outfile_viz << marker.pose.position.x << " "<< marker.pose.position.y << " " << marker.pose.position.z << std::endl;
+            outfile_viz << marker.pose.orientation.x << " "<< marker.pose.orientation.y << " " << marker.pose.orientation.z << " "
+                << marker.pose.orientation.w << std::endl;
+            outfile_viz << marker.scale.x << " " << marker.scale.y << " " << marker.scale.z << std::endl;
+            outfile_viz << marker.color.r << " "<< marker.color.r << " " << marker.color.r << " " << marker.color.a << std::endl;
+            // 记录3D路径车辆数据
+            // Index为2，第一排为位置信息xyz，第二排为缩放信息xyz，第三排为方向信息xyzw，第三排为颜色信息rgb
+        }else{
+            outfile_viz << "3" << std::endl;
+            for (size_t i = 0; i < marker.points.size(); i += 2) {
+                const auto& point = marker.points[i];
+                outfile_viz << point.x << " " << point.y << " " << point.z << std::endl;
+            }
+        }
+        }
+    // 记录3D路径车辆数据
+    // Index为2，第一排为位置信息xyz，第二排为缩放信息xyz，第三排为方向信息xyzw，第三排为颜色信息rgb
+}
+
+void GoalPointPublisher::path2DNodesCallback(const visualization_msgs::MarkerArray::ConstPtr& msg) {
+    // outfile<<"3 "<<std::endl;
+    // for (const auto& marker : msg->markers) {
+    //     outfile_viz << "3" << std::endl;
+    //     outfile_viz << marker.pose.position.x << " "<< marker.pose.position.y << " " << marker.pose.position.z << std::endl;
+    //     outfile_viz << marker.scale.x << " " << marker.scale.y << " " << marker.scale.z << std::endl;
+    //     outfile_viz << marker.color.r << " "<< marker.color.r << " " << marker.color.r << " " << marker.color.a << std::endl;
+    // }
+    // 记录2D路径节点信息
+    // Index为3，第一排为位置信息xyz，第二排为缩放信息xyz，第三排为颜色信息rgba
+}
+
+void GoalPointPublisher::pathBoxesCallback(const visualization_msgs::MarkerArray::ConstPtr& msg) {
+    // for (const auto& marker : msg->markers) {
+    //     outfile_viz << "4" << std::endl;
+    //     outfile_viz << marker.pose.position.x << " "<< marker.pose.position.y << " " << marker.pose.position.z << std::endl;
+    //     outfile_viz << marker.scale.x << " " << marker.scale.y << " " << marker.scale.z << std::endl;
+    //     outfile_viz << marker.color.r << " "<< marker.color.r << " " << marker.color.r << " " << marker.color.a << std::endl;
+    // }
+    // 记录2D搜索圆数据
+    // Index为3，第一排为位置信息xyz，第二排为缩放信息xyz，第三排为颜色信息rgba
+
+}
+
 
 int main(int argc, char** argv)
 {
